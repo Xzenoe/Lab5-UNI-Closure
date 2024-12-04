f55ea17b105a75f971c46a434a028c049abad683
Xzenoe <bogdantepajkin@gmail.com>
2024-12-04 18:05:44 +0200

    f

--- Exercises/2-array.js
+++ Exercises/2-array.js
@@ -1,5 +1,9 @@
 'use strict';
-
-const array = () => null;
-
+const array = () => {
+  const array = [];
+  const current = (x) => array[x];
+  current.pop = () => array.pop();
+  current.push = (x) => array.push(x);
+  return current;
+};
 module.exports = { array };
